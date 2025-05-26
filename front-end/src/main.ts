efb72c0062279cd91698159b67b8c3bb0b08dc06
karlo-kasi <karlokasi1995@gmail.com>
2025-05-26 17:27:32 +0200

    milestone 3

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,48 +1,66 @@
+import type { Actress } from "./type";
 
-/*📌 Milestone 1
-Crea un type alias Person per rappresentare una persona generica.
+function isActress(data: unknown): data is Actress {
+    if (
+        data &&
+        typeof data === "object" &&
+        "id" in data &&
+        typeof data.id === "number" &&
+        "name" in data &&
+        typeof data.name === "string" &&
+        "birth_year" in data &&
+        typeof data.birth_year === "number" &&
+        (!('death_year' in data) || typeof data.death_year === 'number') &&
+        "biography" in data &&
+        typeof data.biography === "string" &&
+        "image" in data &&
+        typeof data.image === "string" &&
+        "most_famous_movies" in data &&
+        data.most_famous_movies instanceof Array &&
+        data.most_famous_movies.length === 3 &&
+        data.most_famous_movies.every((m) => typeof m === "string") &&
+        "awards" in data &&
+        typeof data.awards === "string" &&
+        "nationality" in data &&
+        typeof data.nationality === "string"
+    ) {
+        return true
+    }
 
-Il tipo deve includere le seguenti proprietà:
-
-id: numero identificativo, non modificabile
-name: nome completo, stringa non modificabile
-birth_year: anno di nascita, numero
-death_year: anno di morte, numero opzionale
-biography: breve biografia, stringa
-image: URL dell'immagine, stringa*/
-
-type Person = {
-    readonly id: number,
-    readonly name: string,
-    birth_year: number,
-    death_year?: number,
-    biography: string,
-    image: string
+    return false
 }
 
-/*📌 Milestone 2
-Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+async function getActress(id: number): Promise<Actress | null> {
+    try {
+        const response = await fetch(`http://localhost:3333/actresses/${id}`)
 
-most_famous_movies: una tuple di 3 stringhe
-awards: una stringa
-nationality: una stringa tra un insieme definito di valori.
-Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.*/
+        if (!response.ok) {
+            throw new Error(`Errore HTTP: ${response.status}, ${response.statusText}`)
+        }
+        const data = await response.json()
 
-type ActressNazionality = 
-    | "American"
-    | "British"
-    | "Australian"
-    | "Israeli-American"
-    | "South African"
-    | "French"
-    | "Indian"
-    | "Israeli"
-    | "Spanish"
-    | "South Korean"
-    | "Chinese"
+        if (!isActress(data)) {
+            console.error("Formato di dati non valido:", data);
+            throw new Error(`Formato di dati non valido`)
+        }
 
-type Actress = Person & {
-    most_famous_movies: [string, string, string],
-    awards: string,
-    nationality: ActressNazionality,
+        return data
+
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("Errore durante il recupero del dato", error);
+        }
+        // Restituisci null in caso di errore
+        return null;
+    }
+
 }
+
+
+(async () => {
+
+    const resolve = await getActress(2)
+
+    console.log(resolve)
+
+})()
---
+++ front-end/src/type.ts
@@ -0,0 +1,49 @@
+/*📌 Milestone 1
+Crea un type alias Person per rappresentare una persona generica.
+
+Il tipo deve includere le seguenti proprietà:
+
+id: numero identificativo, non modificabile
+name: nome completo, stringa non modificabile
+birth_year: anno di nascita, numero
+death_year: anno di morte, numero opzionale
+biography: breve biografia, stringa
+image: URL dell'immagine, stringa*/
+
+type Person = {
+    readonly id: number,
+    readonly name: string,
+    birth_year: number,
+    death_year?: number,
+    biography: string,
+    image: string
+}
+
+/*📌 Milestone 2
+Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+
+most_famous_movies: una tuple di 3 stringhe
+awards: una stringa
+nationality: una stringa tra un insieme definito di valori.
+Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.*/
+
+ type ActressNazionality = 
+    | "American"
+    | "British"
+    | "Australian"
+    | "Israeli-American"
+    | "South African"
+    | "French"
+    | "Indian"
+    | "Israeli"
+    | "Spanish"
+    | "South Korean"
+    | "Chinese"
+
+type Actress = Person & {
+    most_famous_movies: [string, string, string],
+    awards: string,
+    nationality: ActressNazionality,
+}
+
+export type { Person, ActressNazionality, Actress };
